63e937e336283ebeeeee4d6960e59fe9d152f013
WIlliam Tomé <william.tomenh@gmail.com>
2019-04-01 17:28:55 -0300

    Salvar os todos no Local Storage.

--- js/todos.js
+++ js/todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input')
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-  'Fazer café',
-  'Estudar JS',
-  'Acessar comunidade'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
   listElement.innerHTML = '';
@@ -38,6 +34,7 @@ function addTodo() {
   todos.push(todoText);
   inputElement.value = '';
   renderTodos();
+  saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -45,4 +42,9 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
   todos.splice(pos, 1);
   renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('list_todos', JSON.stringify(todos));
 }
